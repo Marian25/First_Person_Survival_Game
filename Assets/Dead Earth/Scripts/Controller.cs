fefc2ce8112ab3e6f91291a4ef958e74b774587d
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-12-14 13:58:00 +0200

    Blending Attack Animations with Run & Walk

---
+++ Assets/Dead Earth/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private Animator animator = null;
+    private int horizontalHash = 0;
+    private int verticalHash = 0;
+    private int attackHash = 0;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        horizontalHash = Animator.StringToHash("horizontal");
+        verticalHash = Animator.StringToHash("vertical");
+        attackHash = Animator.StringToHash("attack");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xAxis = Input.GetAxis("Horizontal") * 2.32f;
+        float yAxis = Input.GetAxis("Vertical") * 5.66f;
+
+        if (Input.GetMouseButtonDown(0)) {
+            animator.SetTrigger(attackHash);
+        }
+
+        animator.SetFloat(horizontalHash, xAxis, 0.1f, Time.deltaTime);
+        animator.SetFloat(verticalHash, yAxis, 1.0f, Time.deltaTime);
+	}
+}
---
+++ Assets/Dead Earth/Scripts/SpeedController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour {
+
+    public float speed = 0.0f;
+
+    private Animator controller = null;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        controller.SetFloat("speed", speed);
+	}
+}
