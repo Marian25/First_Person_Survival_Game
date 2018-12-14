9b28d71872d066b2573e822a4d7a4b6e98ce692d
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-12-14 12:33:32 +0200

    Blending Walk & Run Animations

---
+++ Assets/Dead Earth/Scripts/SmoothCameraMount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraMount : MonoBehaviour {
+
+    public Transform mount = null;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, mount.position, Time.deltaTime * speed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, mount.rotation, Time.deltaTime * speed);
+    }
+}
