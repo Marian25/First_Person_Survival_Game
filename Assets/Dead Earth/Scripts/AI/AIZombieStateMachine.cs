16a174c78c5556be0a12e7f9bd91903acde60a17
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-12-23 14:28:01 +0200

    Feeding Zombie State Created

--- Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
+++ Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
@@ -11,6 +11,8 @@ public class AIZombieStateMachine : AIStateMachine {
     [SerializeField] [Range(0, 100)] private int _health = 100;
     [SerializeField] [Range(0f, 1f)] private float _intelligence = 0.5f;
     [SerializeField] [Range(0f, 1f)] private float _satisfaction = 1.0f;
+    [SerializeField] float _replenishRate = 0.5f;
+    [SerializeField] float _depletionRate = 0.1f;
 
     private int _seeking = 0;
     private bool _feeding = false;
@@ -25,12 +27,13 @@ public class AIZombieStateMachine : AIStateMachine {
     private int attackHash = Animator.StringToHash("attack");
 
     // Getters
+    public float replenishRate  { get { return _replenishRate; } }
     public float fov            { get { return _fov; } }
     public float sight          { get { return _sight; } }
     public float hearing        { get { return _hearing; } }
     public bool  crawling       { get { return _crawling; } }
     public float intelligence   { get { return _intelligence; } }
-    public float satisfaction    { get { return _satisfaction; } set { _satisfaction = value; } }
+    public float satisfaction   { get { return _satisfaction; } set { _satisfaction = value; } }
     public float aggression     { get { return _aggression; } set { _aggression = value; } }
     public int health           { get { return _health; } set { _health = value; } }
     public int attackType       { get { return _attackType; } set { _attackType = value; } }
---
+++ Assets/Dead Earth/Scripts/AI/AIZombieState_Feeding1.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIZombieState_Feeding1 : AIZombieState {
+
+    [SerializeField] float slerpSpeed = 5f;
+
+    private int eatingStateHash = Animator.StringToHash("Feeding State");
+    private int eatingLayerIndex = -1;
+
+
+    public override AIStateType GetStateType()
+    {
+        return AIStateType.Feeding;
+    }
+
+    public override void OnEnterState()
+    {
+        base.OnEnterState();
+        Debug.Log("Entering Feeding State");
+
+        if (zombieStateMachine == null) return;
+
+        if (eatingLayerIndex == -1)
+        {
+            eatingLayerIndex = zombieStateMachine.GetAnimator.GetLayerIndex("Cinematic Layer");
+        }
+
+        zombieStateMachine.feeding = true;
+        zombieStateMachine.seeking = 0;
+        zombieStateMachine.speed = 0;
+        zombieStateMachine.attackType = 0;
+
+        zombieStateMachine.NavAgentControl(true, false);
+    }
+
+    public override void OnExitState()
+    {
+        base.OnExitState();
+
+        if (zombieStateMachine != null)
+            zombieStateMachine.feeding = false;
+    }
+
+    public override AIStateType OnUpdate()
+    {
+        if (zombieStateMachine.satisfaction > 0.9f)
+        {
+            zombieStateMachine.GetWaypointPosition(false);
+            return AIStateType.Alerted;
+        }
+
+        if (zombieStateMachine.visualThreat.GetType != AITargetType.None && zombieStateMachine.visualThreat.GetType != AITargetType.Visual_Food)
+        {
+            zombieStateMachine.SetTarget(zombieStateMachine.visualThreat);
+            return AIStateType.Alerted;
+        }
+
+        if (zombieStateMachine.audioThreat.GetType == AITargetType.Audio)
+        {
+            zombieStateMachine.SetTarget(zombieStateMachine.audioThreat);
+            return AIStateType.Alerted;
+        }
+
+        if (zombieStateMachine.GetAnimator.GetCurrentAnimatorStateInfo(eatingLayerIndex).shortNameHash == eatingStateHash)
+        {
+            zombieStateMachine.satisfaction = Mathf.Min(zombieStateMachine.satisfaction + Time.deltaTime * zombieStateMachine.replenishRate / 100, 1.0f);
+        }
+
+        if (!zombieStateMachine.useRootRotation)
+        {
+            Vector3 targetPos = zombieStateMachine.targetPosition;
+            targetPos.y = zombieStateMachine.transform.position.y;
+            Quaternion newRot = Quaternion.LookRotation(targetPos - zombieStateMachine.transform.position);
+            zombieStateMachine.transform.rotation = Quaternion.Slerp(zombieStateMachine.transform.rotation, newRot, Time.deltaTime * slerpSpeed);
+        }
+
+        return AIStateType.Feeding;
+    }
+
+}
